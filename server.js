d49873f09ee93c61ceee28cf843caa25831d64e0
anahtreek <keerthana.n_23@yahoo.com>
2017-09-13 18:20:42 +0530

    server.js change

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
     app     = express(),
-    eps     = require('ejs'),
     morgan  = require('morgan');
 
 Object.assign=require('object-assign')
 
-app.engine('html','css','js', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
